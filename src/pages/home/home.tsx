353f1f8713e02f86716b1cd36cceba6abae17e9b
unknown <ssaifrocks@gmail.com>
2023-03-28 05:22:23 +0530

    Changes

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -11,6 +11,15 @@ export default function Home() {
             <section className="main-container">
                 <NamajTimingForm />
                 <UpcomingPrayerTime />
+                <h3 className="page-title">Hadith of the Day</h3>
+                <div style={{ height: 220,  textAlign: "center" }}>
+                    <iframe
+                        src="https://www.alim.org/hadith-of-the-day/widget/"
+                        height="100%"
+                        width="100%"
+                        // style={{ border: "none" }}
+                    />
+                </div>
             </section>
         </React.Fragment>
     )
