ebce04b819177bbf4406ac51bf40a79361111130
unknown <ssaifrocks@gmail.com>
2023-10-30 13:09:14 +0530

    Changes

--- src/pages/home/components/UpcomingPrayerTime.tsx
+++ src/pages/home/components/UpcomingPrayerTime.tsx
@@ -108,10 +108,7 @@ export default function UpcomingPrayerTime({type}:any) {
                 <>
                     {getCurrentWaqt()!="Tahajjud" && 
                         <>
-                            <h3 className="page-title">Current Prayer Rakats</h3>
-                            <div className="content text-white">
-                                Waqt: <span>{getCurrentWaqt()}</span>
-                            </div>
+                            <h3 className="page-title">{getCurrentWaqt()} Prayer Rakats</h3>
                             {namaj[getCurrentWaqt()].map((x:any, i: any)=>
                                 <div className="content text-white" key={i}>
                                     {parse(x)}
